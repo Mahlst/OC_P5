afa8617cead39e5a4d47a6b38bab56afc941b84e
Mahlst <peter.rothlaender.jr@gmail.com>
2024-08-29 19:03:28 +0200

    Create Card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import './Card.scss'
+
+export default function Card(props) {
+    return (
+        <article className="home__card">
+            <Link className="home__card__link">
+                <img className="home__card__img" src={props.cover} alt={props.title} />
+                <h3 className="home__card__title">{props.title}</h3>
+            </Link>
+        </article>
+    )
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from 'react';
 import Header from "../../components/Header/Header";
 import Banner from "../../components/Banner/Banner";
 import image from '../../assets/images/banner-home.jpg';
 import Footer from "../../components/Footer/Footer";
+import Card from "../../components/Card/Card";
+import './Home.scss'
 
 export default function Home() {
+    const [logements, setLogements] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/logements.json')
+            .then(response => response.json())
+            .then(data => setLogements(data))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
+
     return (
         <>
             <Header />
             <Banner title="Chez vous, partout et ailleurs" image={image} alt="Photographie d'un bord de falaise"/>
+            <main>
+                <section className="home__section">
+                    {logements.map(logement => (
+                        <Card key={logement.id} id={logement.id} title={logement.title} cover={logement.cover} />
+                    ))}
+                </section>
+            </main>
             <Footer />
         </>
     )
