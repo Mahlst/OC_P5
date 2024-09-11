b6c14f36ca73815a72606d293749ef6a6eb439b6
Mahlst <peter.rothlaender.jr@gmail.com>
2024-09-12 00:20:25 +0200

    Add useLocation to Header
    + fix margin

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
-// Importation du composant Link depuis react-router-dom pour la navigation
-import { Link } from "react-router-dom"
+// Importation des hooks useLocation et Link depuis react-router-dom pour la navigation
+import { Link, useLocation } from "react-router-dom"
 // Importation du fichier CSS pour les styles du composant
 import './Header.scss'
 // Importation du logo depuis le chemin relatif
@@ -7,22 +7,22 @@ import logo from '../../assets/images/logo.svg'
 
 // Déclaration et exportation du composant fonctionnel Header
 export default function Header() {
+    // Utilisation du hook useLocation pour obtenir le chemin actuel
+    const location = useLocation();
+
     return (
         // Élément header avec la classe CSS 'header'
         <header className="header">
             <h1>
-                {/* Composant Link pour créer un lien vers la page d'accueil */}
-                <Link to="/">
-                    {/* Élément img avec la classe CSS 'logo', la source de l'image du logo et le texte alternatif */}
-                    <img className="logo" src={logo} alt="Logo Kasa"/>
-                </Link>
+                {/* Élément img avec la classe CSS 'logo', la source de l'image du logo et le texte alternatif */}
+                <img className="logo" src={logo} alt="Logo Kasa"/>
             </h1>
             {/* Élément nav pour la navigation */}
             <nav className="nav">
-                {/* Lien de navigation vers la page d'accueil */}
-                <Link className="nav__link" to="/">Accueil</Link>
-                {/* Lien de navigation vers la page 'A Propos' */}
-                <Link className="nav__link" to="/about">A Propos</Link>
+                {/* Lien de navigation vers la page d'accueil avec condition pour ajouter la classe 'active' */}
+                <Link className={`nav__link ${location.pathname === '/' ? 'active' : ''}`} to="/">Accueil</Link>
+                {/* Lien de navigation vers la page 'A Propos' avec condition pour ajouter la classe 'active' */}
+                <Link className={`nav__link ${location.pathname === '/about' ? 'active' : ''}`} to="/about">A Propos</Link>
             </nav>
         </header>
     )
