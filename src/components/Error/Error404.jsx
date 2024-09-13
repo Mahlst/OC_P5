93077dd081252fc0709f9d622610c73c468940a3
Mahlst <peter.rothlaender.jr@gmail.com>
2024-09-13 15:20:20 +0200

    Fix h2 Error404

--- src/components/Error/Error404.jsx
+++ src/components/Error/Error404.jsx
@@ -9,7 +9,7 @@ export default function Error404() {
         // Élément main avec la classe CSS 'main__404'
         <main className="main__404">
             {/* Élément h1 avec la classe CSS 'title__404' pour afficher le code d'erreur 404 */}
-            <h1 className="title__404">404</h1>
+            <h2 className="title__404">404</h2>
             {/* Élément p avec la classe CSS 'text__404' pour afficher le message d'erreur */}
             <p className="text__404">Oups! La page que vous demandez n'existe pas.</p>
             {/* Composant Link pour retourner à la page d'accueil */}
